6f1ff144002240328e4d6bf7fc082b9cde3bc8fe
reyhzer <nextconsol@gmail.com>
2026-04-14 16:14:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
+    tsconfigPaths(),
     tailwindcss(),
-    netlify(),
     tanstackStart(),
-    viteReact(),
+    react(),
   ],
 })
-
-export default config
